6091bd20b4ab4e65498c40deb7601c809fc19c74
dgho <dalia_ghoneim@urmc.rochester.edu>
2014-01-08 10:39:09 -0500

    fix missing global variable, add -d to usage explanation

--- print_2_vcf.pl
+++ print_2_vcf.pl
@@ -4,7 +4,7 @@
 #10/25/2013 
 #takes a list of genes and a vcf. Prints out lines in the vcf that match the genes in the list and that have the indicated genotype.
 #in this case, the pedigree is CH
-#to run: perl <gene_list> <snv_annotated_with_annovar>.vcf
+#to run: perl print_2_vcf.pl <gene_list> <snv_annotated_with_annovar>.vcf
 #
 #Dalia Ghoneim
 use strict;
@@ -13,6 +13,7 @@ use Getopt::Long;
 my $aff_geno_match;
 my $proband_index=35;
 my $NUM_AFFECTED=1;
+my $i;
 GetOptions ('NUM_AFFECTED:i'=>\$NUM_AFFECTED, 'PROBAND:i'=>\$proband_index);
 my $file1 = shift;
 my $file2 = shift;
@@ -38,6 +39,7 @@ LINE: while ($_=<$F2>){
         my @line = split /\t/;
         if (exists($genes_list{$line[1]})&&($line[$proband_index] =~ m{0/1})&&($line[$father_index] =~m{0/0}) && ($line[$mother_index] =~ m{0/1})){
 		#handle multiple affecteds
+		$aff_geno_match=1;
 		for($i=1;$i<$NUM_AFFECTED;$i++){
 			if ($line[$proband_index+$i]=~m{0/1}){
 				$aff_geno_match++;
@@ -49,6 +51,7 @@ LINE: while ($_=<$F2>){
 	}
         elsif (exists($genes_list{$line[1]})&&($line[$proband_index] =~ m{0/1})&&($line[$father_index] =~m{0/1}) && ($line[$mother_index] =~ m{0/0})){
        		#handle multiple affecteds
+		$aff_geno_match=1;
 		for($i=1;$i<$NUM_AFFECTED;$i++){
 			if ($line[$proband_index+$i]=~m{0/1}){
 				$aff_geno_match++;
